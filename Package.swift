2c8cd3d27058af106f34ff291bd5e3ffb41f7b2f
smalls <smalls0098@gmail.com>
2025-01-01 13:23:39 +0800

    init Package.swift

--- Package.swift
+++ Package.swift
@@ -1,22 +1,24 @@
 // swift-tools-version:5.3
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
 import PackageDescription
 
 let package = Package(
     name: "LYEmptyView",
-    platforms: [
-        .iOS(.v9)
-    ],
     products: [
-        .library(
-            name: "LYEmptyView",
-            targets: ["LYEmptyView"]),
+        .library(name: "LYEmptyView", targets: ["LYEmptyView"]),
     ],
+    dependencies: [],
     targets: [
         .target(
             name: "LYEmptyView",
+            dependencies: [],
             path: "LYEmptyView",
-            publicHeadersPath: "."
-        )
-    ],
-    swiftLanguageVersions: [.v5]
+            publicHeadersPath: "include",
+            cSettings: [
+                //Config header path
+                .headerSearchPath("."),
+            ]
+        ),
+    ]
 )
